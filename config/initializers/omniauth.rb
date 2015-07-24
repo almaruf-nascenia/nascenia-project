34651c182eedafaae52c198ecb305484b249739e
Swawibe Alam <swawibe@bdipo.com>
2015-07-24 16:04:00 +0600

    Google New application made and omniauth key and token updated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env == "development"
-    provider :google_oauth2, "422289888657-pt064766l8lt97f965hi9d298pskpm3a.apps.googleusercontent.com", "pfT0D9p2AJxXCGAyzEbSvplJ"
-  elsif Rails.env == "staging"
-    provider :google_oauth2, "422289888657-pt064766l8lt97f965hi9d298pskpm3a.apps.googleusercontent.com", "pfT0D9p2AJxXCGAyzEbSvplJ"
+  if Rails.env.development?
+    provider :google_oauth2, '859503770764-bpatuj1j8prr1gadqgeid6lflls4esru.apps.googleusercontent.com', 'AhkPDlFTXff-lfItWGJf4X8I'
+  elsif Rails.env.staging?
+    provider :google_oauth2, '531476693694-p75kjs26m538nfdf4qa5v5q49ddnq4rs.apps.googleusercontent.com', 'bx8atFsQrsnnp_Ke7Ku8FLV3'
   end
 end
