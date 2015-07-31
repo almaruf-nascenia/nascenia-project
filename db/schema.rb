8fd8db489c8ab376f653172bef9693d57e08003b
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-07-31 15:03:58 +0600

    - change developer remove (from project) confirm button style
    - change text submit to Remove and make button red
    - Change 'remove' text color (modal header) to bootstrap danger

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726064421) do
+ActiveRecord::Schema.define(version: 20150731085219) do
 
   create_table "developers", force: true do |t|
     t.string   "name"
